7f2f6552c0f90be716f9e461c50608fc2f8c047d
julianPrieto1323 <9000169@alumnos.ufv.es>
2023-03-03 14:54:28 +0100

    Comienzo a programar las paginas de registro

---
+++ Frontend/src/main/java/org/vaadin/example/Registrer.java
@@ -0,0 +1,9 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class Register extends VerticalLayout {
+
+}
+
