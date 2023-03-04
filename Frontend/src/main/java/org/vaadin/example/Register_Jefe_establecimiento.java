b4e77059d981c02817bb8fa3f98a6d00658ac771
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-03-04 19:12:36 +0100

    Arreglado Vertical Layout

--- Frontend/src/main/java/org/vaadin/example/Register_Jefe_establecimiento.java
+++ Frontend/src/main/java/org/vaadin/example/Register_Jefe_establecimiento.java
@@ -6,6 +6,8 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
@@ -21,27 +23,28 @@ public class Register_Jefe_establecimiento extends Div
     public void build()
     {
         // Configurar componentes
-        VerticalLayout V = new VerticalLayout();
+        VerticalLayout Vertical = new VerticalLayout();
         H1 title = new H1("Resgistrarse Jefe de establecimiento");
         TextField nombre = new TextField("Nombre");
         TextField apellido = new TextField("Apellido");
         PasswordField contrasena = new PasswordField("Contraseña");
         PasswordField confirmarContrasena = new PasswordField("Confirmar contraseña");
-        TextField correo = new TextField("Correo Electrónico");
+        EmailField correo = new EmailField("Correo Electrónico");
         TextField direccion = new TextField("Dirección de la calle");
         TextField CP = new TextField("Codigo Postal");
-        TextField telefono = new TextField("Teléfono");
+        NumberField telefono = new NumberField("Teléfono");
         TextField cif = new TextField("CIF");
         Button registerButton = new Button("Registrarse"); // Usar com.vaadin.flow.component.button.Button
 
         // Agregar componentes al layout vertical
-        V.setAlignItems(FlexComponent.Alignment.CENTER);
-        V.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
-        add(title, nombre, apellido, contrasena, confirmarContrasena,correo,direccion,CP,telefono,cif, registerButton);
 
+        Vertical.add(title, nombre, apellido, contrasena, confirmarContrasena,correo,direccion,CP,telefono,cif, registerButton);
 
-        // Configurar layout
+        Vertical.setAlignItems(FlexComponent.Alignment.CENTER);
+        Vertical.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
 
+        add(Vertical);
+        //layout.addComponent(Vertical);
         // Configurar acciones de los componentes
         registerButton.addClickListener(event -> register());
     }
