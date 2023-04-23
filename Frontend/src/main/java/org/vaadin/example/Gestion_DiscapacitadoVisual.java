1ec19a5c11874610419052388fe50fcd71d9be9c
Jmrx24 <Jmrx24@gmail.com>
2023-04-23 14:04:33 +0200

    clase DiscapacitadoVisual editada

--- Frontend/src/main/java/org/vaadin/example/Gestion_DiscapacitadoVisual.java
+++ Frontend/src/main/java/org/vaadin/example/Gestion_DiscapacitadoVisual.java
@@ -1,5 +1,6 @@
 package org.vaadin.example;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -7,6 +8,9 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.StreamResource;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Route("gestion_DiscapacitadoVisual")
 @Theme(value = Lumo.class, variant = Lumo.LIGHT)
@@ -25,6 +29,25 @@ public class Gestion_DiscapacitadoVisual extends VerticalLayout {
         H1 tit = new H1("Establecimientos visitados");
         tit.addClassName("tit_admin");
 
+        // Generar la tabla con los campos arriba puestos.
+        Grid<Discapacitado_VIsual> grid = new Grid<>(Discapacitado_VIsual.class, false);
+        grid.addColumn(Discapacitado_VIsual::getName).setHeader("Nombre");
+        grid.addColumn(Discapacitado_VIsual::getApellido).setHeader("Apellido");
+        grid.addColumn(Discapacitado_VIsual::getEmail).setHeader("Email");
+        grid.addColumn(Discapacitado_VIsual::getEdificios).setHeader("Edificios");
+        grid.addColumn(Discapacitado_VIsual::getMapa).setHeader("Mapas");
+
+        //Rellenno el arrayilst, con los datos recibidos
+        /*try {
+            listaEstablecimientos = DataService.obtenerListaEstablecimientos();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        grid.setItems(listaEstablecimientos);*/
+
+        add(img,tit,grid);
 
     }
 }
